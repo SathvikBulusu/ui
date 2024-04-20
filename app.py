8ee5da845b98dc723acd2002134e85da0e229d3c
Sathvik_Bulusu <bulususathvik7890@gmail.com>
2024-04-21 01:20:15 +0530

    i have integrated towards google drive

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-import streamlit as st 
+import streamlit as st
+from google.oauth2 import service_account
+from googleapiclient.discovery import build  
+from googleapiclient.http import MediaIoBaseUpload  # Import MediaIoBaseUpload
+import io  # Import io module for byte I/O support
 import pandas as pd 
+
+
+creds = service_account.Credentials.from_service_account_file(r'D:\bots.json',scopes=['https://www.googleapis.com/auth/drive'])
+
+drive_service = build('drive','v3',credentials=creds)
 
 # Retrieve the maximum upload size from the configuration file
 max_upload_size_mb = st.get_option("server.maxUploadSize")
@@ -17,7 +26,7 @@ def user_input_features():
 
     up_files = st.sidebar.file_uploader("Upload up to 6 files", accept_multiple_files=True, type=['csv', 'xls', 'xlsx'])
 
-    uploaded_file_names = []  # List to store uploaded file names
+    uploaded_files_metadata = []  # List to store uploaded file metadata
 
     if up_files is not None:
         for up_file in up_files:
@@ -32,14 +41,29 @@ def user_input_features():
                     if total_size_mb > max_upload_size_mb:
                         st.sidebar.warning(f"Maximum total size of uploaded files reached. You have exceeded {max_upload_size_mb} MB.")
                         break
-                    uploaded_file_names.append(filename)  # Store filename
 
-    return uploaded_file_names
+                    # Create a media upload object for the file content
+                    media = MediaIoBaseUpload(io.BytesIO(up_file.getvalue()), mimetype=up_file.type, resumable=True)
 
-uploaded_file_names = user_input_features()
+                    # Upload file to Google Drive
+                    file_metadata = {
+                        'name': filename,
+                        # 'parents': [folder_id],  # Optionally specify folder ID if you want to save files in a specific folder
+                    }
+                    media = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
 
-# Display uploaded file names
-if uploaded_file_names:
+                    # Store file metadata
+                    uploaded_files_metadata.append({
+                        'name': filename,
+                        'id': media.get('id')
+                    })
+
+    return uploaded_files_metadata
+
+uploaded_files_metadata = user_input_features()
+
+# Display uploaded file names and their corresponding Google Drive IDs
+if uploaded_files_metadata:
     st.write("## Uploaded Files:")
-    for filename in uploaded_file_names:
-        st.write(filename)
+    for file_metadata in uploaded_files_metadata:
+        st.write(f"Name: {file_metadata['name']}, ID: {file_metadata['id']}")
