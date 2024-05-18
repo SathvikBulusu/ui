f44dca7f2c8d44b13000e2ae36e01405fde0ceb8
Sathvik_Bulusu <bulususathvik7890@gmail.com>
2024-05-18 15:18:57 +0530

    saving the data from the real time data of apis

--- finance.py
+++ finance.py
@@ -3,18 +3,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from alpha_vantage.timeseries import TimeSeries
 import time
-
+import psycopg2
+from psycopg2 import sql
 
 def fetch_stock_prices(symbol, api_key):
     ts = TimeSeries(key=api_key, output_format='pandas')
     try:
-        data, meta_data = ts.get_intraday(symbol=symbol, interval='30min', outputsize='compact')
+        data, meta_data = ts.get_intraday(symbol=symbol, interval='5min', outputsize='compact')
         return data
     except Exception as e:
         st.error(f"Error fetching data: {e}")
         return None
 
+#storing data in postgres
+def storing_data_posgres(data,symbol):
+    conn = psycopg2.connect(
+        host="localhost",
+        user="postgres",
+        database="stock_data",
+        password="1234"
+    )
+    cur=conn.cursor()
 
+    for index,row in data.iterrows():
+        query=sql.SQL("INSERT INTO stock_price(symbol,timestamps,price)VALUES(%s,%s,%s)")
+        cur.execute(query,(symbol,index,row['4. close']))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+#function to plot graph 
 def plotstocks(data,symbol):
     fig,ax=plt.subplots()
     ax.plot(data.index, data['4. close'], label=f'{symbol} close price')
@@ -26,8 +45,7 @@ def plotstocks(data,symbol):
     st.pyplot(fig)
 
 
-
-
+#main function that executes the showcasing of realtime data 
 def main ():
     st.title("Real time streaming data of intraday")
     
@@ -45,10 +63,12 @@ def main ():
 
                 plotstocks(data,symbol)
 
+                storing_data_posgres(data,symbol)
+
                 st.write("Data table:")
                 st.write(data)
-            time.sleep(1800)
-
+            time.sleep(300)
+    
 if __name__ == "__main__":
     main()
 
